d4df5f230b8be2f46c660379c89f37f1f9a4ceec
mariam0ashraf <mariam00ashraf@gmail.com>
2024-10-17 15:31:16 -0400

    ICA last updatee

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -8,15 +8,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if( (supportFragmentManager.findFragmentById(R.id.fragmentContainerView) is DieFragment)){
 
+        }
+        else {
+            DieFragment.newInstance(6)
+            (supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as DieFragment).throwDie();
+
+        }
         findViewById<Button>(R.id.rollDiceButton).setOnClickListener {
-            if( (supportFragmentManager.findFragmentById(R.id.fragmentContainerView) is DieFragment)){
-
-            }
-            else{
-                DieFragment.newInstance(6)
-                (supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as DieFragment).throwDie();
-            }
+            (supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as DieFragment).throwDie();
 
         }
     }
