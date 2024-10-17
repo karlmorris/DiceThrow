8a7f90f2f886f125cf9729f54dfabb501d2fd795
mariam0ashraf <mariam00ashraf@gmail.com>
2024-10-17 14:57:08 -0400

    in class activity

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -1,26 +1,17 @@
 package edu.temple.dicethrow
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
-import androidx.fragment.app.FragmentTransaction
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var rollButton: Button
-    private lateinit var dieFragment: DieFragment
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        rollButton=findViewById(R.id.rollDiceButton)
-        val fragment= supportFragmentManager.findFragmentById(R.id.fragmentContainerView)as? DieFragment
-        rollButton.setOnClickListener {
-            dieFragment.throwDie()
+
+        findViewById<Button>(R.id.rollDiceButton).setOnClickListener {
+            DieFragment.newInstance(6)
+            (supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as DieFragment).throwDie();
         }
-
-
     }
-
-
 }
