7db8f110190dfeff9bdcfbf0f3fd23e18ec9fd4d
John Joseph II <jackjoseph7447@gmail.com>
2024-10-22 12:21:01 -0400

    interface

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,11 +21,9 @@ class MainActivity : AppCompatActivity() {
                 .commit()
         }
 
+    }
 
-        val button = findViewById<Button>(R.id.rollDiceButton).setOnClickListener {
-            (supportFragmentManager.findFragmentById(R.id.diceContainer) as DieFragment).throwDie()
-        }
-
-
+    override fun buttonClicked() {
+        TODO("Not yet implemented")
     }
 }
